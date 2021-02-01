3b9cda1ec31d1082257b4d7a8c8551980ff81a64
Vegard Egeland <vegardegeland@gmail.com>
2021-02-01 17:15:19 +0100

    Add style to chromatic key

--- src/ChromaticKey.js
+++ src/ChromaticKey.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types'
 
 export function ChromaticKey(props) {
 
-  const color = props.isPressed ? '#E84855'
-              : props.isHighlighted ? '#F2929A'
+  const color = props.isPressed ? props.style.pressedColor
+              : props.isHighlighted ? props.style.highlightedColor
               : '#000'
 
   return <path className={`diatonic-key-${props.note}s
@@ -12,13 +12,13 @@ export function ChromaticKey(props) {
                d={`M0 0 l0 190 c0 0 0 10 10 10 l30 0 c0 0 10 0 10 -10 l0 -190 Z`}
                fill={color}
                stroke='#000'
-               strokeWidth={props.strokeWidth} />
+               strokeWidth={props.style.strokeWidth} />
 }
 
 ChromaticKey.propTypes = {
   note: PropTypes.string.isRequired,
   octaveNum: PropTypes.number.isRequired,
-  strokeWidth: PropTypes.number.isRequired,
   isPressed: PropTypes.bool,
   isHighlighted: PropTypes.bool,
+  style: PropTypes.object,
 }

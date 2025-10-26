548d97c6054155e33fbd1d7bc290a9cc219c6639
Vegard Egeland <vegardegeland@gmail.com>
2025-10-26 17:22:26 +0100

    Add `typedoc` and improved documentation

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,26 @@
+/**
+ * @packageDocumentation
+ * Interactive SVG piano component for React.
+ *
+ * A lightweight, stateless, customizable piano component that renders as SVG.
+ * Supports multiple octaves, mouse and keyboard interactions, and flexible styling.
+ *
+ * Created to integrate with the `kamasi` music theory library
+ *
+ * ## Quick Start
+ * ```tsx
+ * import { Piano } from 'diatonic-piano'
+ *
+ * function App() {
+ *   return <Piano />
+ * }
+ * ```
+ *
+ * @see {@link Piano} - Main component
+ * @see {@link PianoProps} - Component props
+ * @see {@link Theme} - Theming system
+ */
+
 export type { NoteInput, PianoProps, StyleOverride } from './Piano.js'
 export { Piano } from './Piano.js'
 export type { KeyStyle, Theme } from './themes.js'
